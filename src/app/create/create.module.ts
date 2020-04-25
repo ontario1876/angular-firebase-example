2f6374c90d07f631c828dd3697cc914a9ae5a644
ontario1876 <ko.nakamura.10.13@gmail.com>
2020-04-25 15:28:46 +0900

    feat(create):ペット選択カルーセル追加

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -8,6 +8,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatRadioModule} from '@angular/material/radio';
+import { SwiperModule } from 'ngx-swiper-wrapper';
 
 @NgModule({
   declarations: [CreateComponent],
@@ -18,7 +19,8 @@ import {MatRadioModule} from '@angular/material/radio';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatRadioModule
+    MatRadioModule,
+    SwiperModule
   ]
 })
 export class CreateModule { }
