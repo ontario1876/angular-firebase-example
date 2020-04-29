eac741cb2d620b6631ab346e9dd476a6e89b2818
ontario1876 <ko.nakamura.10.13@gmail.com>
2020-04-29 11:34:01 +0900

    feat(firestore): データを表示

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,12 +10,16 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class AuthService {
   afUser$: Observable<User> = this.afAuth.user;
+  uid: string;
+
   constructor(
     private afAuth: AngularFireAuth,
     private router: Router,
     private snackBar: MatSnackBar
   ) {
-    this.afUser$.subscribe(user => console.log(user));
+    this.afUser$.subscribe(user => {
+      this.uid = user && user.uid;
+    });
   }
 
   login() {
@@ -25,6 +29,7 @@ export class AuthService {
       this.snackBar.open('ようこそGitPetへ', null, {
         duration: 2000
       });
+      this.router.navigateByUrl('/create');
     });
   }
 
@@ -33,7 +38,7 @@ export class AuthService {
       this.snackBar.open('ログアウトしました', null, {
         duration: 2000
       });
+      this.router.navigateByUrl('/welcome');
     });
-    this.router.navigateByUrl('/welcome');
   }
 }
